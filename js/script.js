3210fb2335f600a90c36899dbe83b1e49d787d3d
varmac <varmac231@gmail.com>
2024-06-06 08:57:53 +0000

    debugging mixed active content error - changed fetch url from http to https

--- js/script.js
+++ js/script.js
@@ -7,21 +7,18 @@ const humidity_percentage = document.querySelector(".humidity-percentage");
 const current_temperature = document.querySelector(".current-temperature");
 let currentWeather, windSpeed, humidity, temperature;
 
-// async function fetchWeatherData(cityName){
-//     const responseGeolacation = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=586d47e17b7dd94ea91ef9f9e6785a3e`, {referrerPolicy: "unsafe-url"});
-//     const geolocationData = await responseGeolacation.json();
-//     const responseWeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geolocationData[0].lat}&lon=${geolocationData[0].lon}&appid=586d47e17b7dd94ea91ef9f9e6785a3e`, {referrerPolicy: "unsafe-url"});
-//     const weatherData = await responseWeatherData.json();
-//     currentWeather = weatherData.weather[0].main.toLowerCase();
-//     windSpeed = weatherData.wind.speed;
-//     humidity = weatherData.main.humidity;
-//     temperature = weatherData.main.temp;
-//     console.log(weatherData)
-// }
+async function fetchWeatherData(cityName){
+    const responseGeolacation = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=586d47e17b7dd94ea91ef9f9e6785a3e`);
+    const geolocationData = await responseGeolacation.json();
+    const responseWeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geolocationData[0].lat}&lon=${geolocationData[0].lon}&appid=586d47e17b7dd94ea91ef9f9e6785a3e`);
+    const weatherData = await responseWeatherData.json();
+    currentWeather = weatherData.weather[0].main.toLowerCase();
+    windSpeed = weatherData.wind.speed;
+    humidity = weatherData.main.humidity;
+    temperature = weatherData.main.temp;
+    console.log(weatherData)
+}
 
 
-// fetchWeatherData("Monrovia")
+fetchWeatherData("Monrovia")
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=Monrovia&limit=5&appid=586d47e17b7dd94ea91ef9f9e6785a3e")
-    .then(response=> response.json())
-    .then(data => console.log(data))
